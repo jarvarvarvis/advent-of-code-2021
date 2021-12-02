00c7fa2b3cb4d68e41b56f73042a892fb017b078
MisterJaJo <jarvistrigo@gmail.com>
2021-12-02 20:07:45 +0100

    Added script to create day-n folders semi-automatically

---
+++ make_day.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ $1 = "help" ]; then
+    echo "make_day.sh [DAY] [SCRIPT_EXTENSION]"
+    exit
+fi;
+
+mkdir "day-$1/"
+
+cd "day-$1/"
+touch day-$1.$2
+touch day-$1-part2.$2
+touch input.txt
